d8664f398321bfe85e60f8b6c8337a14c1e1a37c
yhupaul <yhupaul@hotmail.com>
2022-05-23 00:28:32 -0700

    got right result but not pass on leetcode

---
+++ AddTwoNum.js
@@ -0,0 +1,18 @@
+var addTwoNumbers = function(l1, l2) {
+  let result;
+  let firNum = l1.reverse().join(''); 
+  console.log(firNum)
+  let secNum = l2.reverse().join(''); 
+  console.log(secNum)
+   
+  let total = Number(firNum) + Number(secNum); 
+  console.log(total)
+  result = total.toString().split('').reverse().map(Number);
+  console.log(result)
+};
+
+// l1 = [2,4,3]
+// l2 = [5,6,4]
+
+addTwoNumbers([2,4,3], [5,6,4]);
+
