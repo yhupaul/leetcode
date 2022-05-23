975d1b7f7d45264920aa4afc4831a773cb935db6
yhupaul <yhupaul@hotmail.com>
2022-05-23 00:51:09 -0700

    right answer but not for leetcode

--- AddTwoNum.js
+++ AddTwoNum.js
@@ -1,12 +1,12 @@
 var addTwoNumbers = function(l1, l2) {
   let result;
   let firNum = l1.reverse().join(''); 
-  console.log(firNum)
+  // console.log(firNum)
   let secNum = l2.reverse().join(''); 
-  console.log(secNum)
+  // console.log(secNum)
    
   let total = Number(firNum) + Number(secNum); 
-  console.log(total)
+  // console.log(total)
   result = total.toString().split('').reverse().map(Number);
   console.log(result)
 };
@@ -15,4 +15,6 @@ var addTwoNumbers = function(l1, l2) {
 // l2 = [5,6,4]
 
 addTwoNumbers([2,4,3], [5,6,4]);
+addTwoNumbers([0], [0]);
+addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]);
 
