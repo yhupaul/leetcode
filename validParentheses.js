6dd2064de8c82a9288763002c67c267da37037dd
yhupaul <yhupaul@hotmail.com>
2022-05-30 15:08:59 -0700

    validParents I soleved it but sitll not fully understand

--- validParentheses.js
+++ validParentheses.js
@@ -1,10 +1,22 @@
 isValid = s => {
+  // Create a stack
   let stack = [];
+  // Loop through each element in the string
   for (i=0; i<s.length; i++){
     let char = stack[stack.length -1]
+    // If you encounter a starting bracket, push it onto the stack
     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
       stack.push(s[i]);
-    } else if ((char === "(" && char === ")") || (char === "{" &&)
-    )
+      // Pop the opening bracket off the stack,
+      // if there is a corresponding closing bracket in the string
+    } else if (
+      (char === "(" && s[i] === ")") || 
+      (char === "{" && s[i] === "}") ||
+      (char === "[" && s[i] === "]") 
+    ) {
+      stack.pop();
+    } else return false;
   }
+  // Check empty stack
+  return stack.length ? false : true;
 }
