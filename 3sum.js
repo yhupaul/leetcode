1b65dda0bf71d2c6036f85404907b70271e42a98
yhupaul <yhupaul@hotmail.com>
2022-05-31 15:36:20 -0700

    can't solve it neither understand

---
+++ 3sum.js
@@ -0,0 +1,8 @@
+const threeSum =  function(nums) {
+  const answer = [];
+  // to compare each number by order if sum is too high, add left one vice versa right one. 
+  nums.sort((a, b) => a-b);
+ 
+  for (let main = )
+
+}
