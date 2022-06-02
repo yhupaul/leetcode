2edb5cf7049034c8b613ccb7a452494a317c76ef
yhupaul <yhupaul@hotmail.com>
2022-06-01 17:06:18 -0700

    wrting the solution almost understand

--- 3sum.js
+++ 3sum.js
@@ -1,8 +1,8 @@
 const threeSum =  function(nums) {
   const answer = [];
-  // to compare each number by order if sum is too high, add left one vice versa right one. 
+  // to compare each number by order if sum is too high, add left one vice versa right one. it save the time easy to find dupulication.
   nums.sort((a, b) => a-b);
  
-  for (let main = )
+ 
 
 }
