fb74f099aabc37958b16c0271fadadd37d6872eb
yhupaul <yhupaul@hotmail.com>
2022-06-18 11:37:25 -0700

    so hard to understand.

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -22,10 +22,24 @@ function ListNode(val, next) {
     this.next = (next===undefined ? 0 : next)
 }
 
-const mergeTwoLists =function(list1, list2) {
-    let node;
-    let head;
-    if (list1 === null) {
-        return list2
+var mergeTwoLists =function(list1, list2) {
+    const dummy = new ListNode(); 
+    let prev = dummy;
+
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            prev.next = list1;
+            prev = list1;
+            list1 = list1.next;
+        } else {
+            prev.next = list2;
+            prev = list2;
+            list2 = list2.next
+        }
+
     }
+    if(!list1) prev.next = list2;
+    if(!list2) prev.next = list1;
+
+    return dummy.next;
 }
