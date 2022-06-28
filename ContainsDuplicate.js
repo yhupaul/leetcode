3a6c9ed2255b1f941eb8ffdfe71276e7ddcd7aff
yhupaul <yhupaul@hotmail.com>
2022-06-27 20:46:49 -0700

    solved it with using Set()function on ContainDuplicate

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,12 +1,20 @@
+// var containsDuplicate = function(nums) {
+//   if (nums.length < 1) {
+//     return false;
+//   }
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i+1; j < nums.length; j++){
+//       if(nums[i] === nums[j]){
+//         return true;
+//       } 
+//     }
+//   }return false;
+// };
+
+// use Set();
+
 var containsDuplicate = function(nums) {
-  if (nums.length < 1) {
-    return false;
-  }
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i+1; j < nums.length; j++){
-      if(nums[i] === nums[j]){
-        return true;
-      } 
-    }
-  }return false;
+  let myNums = new Set(nums);
+  if(myNums.size !== nums.length) return true;
+  else return false;
 };
