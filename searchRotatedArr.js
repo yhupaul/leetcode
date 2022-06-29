e7ff848ca7c42386b23d29d4dd2e1de6d21f5361
yhupaul <yhupaul@hotmail.com>
2022-06-28 22:49:19 -0700

    new solution for searchRotatedArr

--- searchRotatedArr.js
+++ searchRotatedArr.js
@@ -1,8 +1,16 @@
+// var search = function(nums, target) {
+//   if(!nums.includes(target)) return -1;
+//   else {
+//       for(let i=0; i<nums.length; i++) {
+//           if(nums[i] === target) return i; 
+//       }
+//   }
+// };
+
 var search = function(nums, target) {
   if(!nums.includes(target)) return -1;
   else {
-      for(let i=0; i<nums.length; i++) {
-          if(nums[i] === target) return i; 
-      }
+    return nums.indexOf(target);        
   }
 };
+console.log(search([4,5,6,7,0,1,2], 0))
