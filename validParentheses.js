eb42eff5f44675f526d25f50a8560fb8b34b802a
yhupaul <yhupaul@hotmail.com>
2022-07-09 15:22:34 -0700

    new solution for vaildparentheses

--- validParentheses.js
+++ validParentheses.js
@@ -1,24 +1,39 @@
-isValid = s => {
-  // Create a stack
-  let stack = [];
-  // Loop through each element in the string
-  for (i=0; i<s.length; i++){
-    let char = stack[stack.length -1]
-    // If you encounter a starting bracket, push it onto the stack
-    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
-      stack.push(s[i]);
-      // Pop the opening bracket off the stack,
-      // if there is a corresponding closing bracket in the string
-    } else if (
-      (char === "(" && s[i] === ")") || 
-      (char === "{" && s[i] === "}") ||
-      (char === "[" && s[i] === "]") 
-    ) {
-      stack.pop();
-    } else return false;
-  }
-  // Check empty stack
-  return stack.length ? false : true;
-}
+// isValid = s => {
+//   // Create a stack
+//   let stack = [];
+//   // Loop through each element in the string
+//   for (i=0; i<s.length; i++){
+//     let char = stack[stack.length -1]
+//     // If you encounter a starting bracket, push it onto the stack
+//     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+//       stack.push(s[i]);
+//       // Pop the opening bracket off the stack,
+//       // if there is a corresponding closing bracket in the string
+//     } else if (
+//       (char === "(" && s[i] === ")") || 
+//       (char === "{" && s[i] === "}") ||
+//       (char === "[" && s[i] === "]") 
+//     ) {
+//       stack.pop();
+//     } else return false;
+//   }
+//   // Check empty stack
+//   return stack.length ? false : true;
+// }
 
-// finally I am able to understand it
+// // finally I am able to understand it
+
+var isValid = function(s) {
+  const hashMap = { "(":")", "{":"}", "[":"]" };
+  const stack = [];
+  for(let ch of s) {
+      if(hashMap[ch]) {
+          stack.push(hashMap[ch]);
+      } else if (stack.length > 0 && stack[stack.length - 1] === ch) {
+          stack.pop();
+      } else {
+          return false;
+      }
+  }
+  return !stack.length;
+};
