b094e00cd5bf43e0c66b7429f30db65f755f4168
yhupaul <yhupaul@hotmail.com>
2022-07-18 23:27:46 -0700

    added new solution for 2d matrix

--- search2DMat.js
+++ search2DMat.js
@@ -18,4 +18,23 @@ var searchMatrix = function(matrix, target) {
   return false;``
 };
 
+//the other way to solve
+var searchMatrix = function(matrix, target) {
+  let fir = 0;
+  let last = matrix.length*matrix[0].length - 1;
+  
+  while(fir <= last) {
+      let mid = fir + Math.floor((last - fir) / 2);
+      col = Math.floor(mid % matrix[0].length);
+      row = Math.floor(mid / matrix[0].length);
+      
+      if (matrix[row][col] === target)
+          return true;
+      else if (matrix[row][col] < target)
+          fir = mid + 1;
+      else
+          last = mid - 1;
+  }
+  return false;
+}
 console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
