bd2d9c53989afaea7ee7e9c50d34238a5f4a58c3
yhupaul <yhupaul@hotmail.com>
2022-09-08 00:01:27 -0700

    re solved

--- binarySearch.js
+++ binarySearch.js
@@ -1,13 +1,3 @@
-// var search(nums, target) {
-//   let start = 0;
-//   let end = nums.length - 1;
-//   let mid = Math.floor((start + end) / 2);
-//   while (nums[mid] !== target && start <= end) {
-//     if(target < nums[mid]) end = mid -1;
-//     else start = mid + 1
-//     mid = 
-//   }
-// }
 
 var search = function(nums, target) {
   let fir = 0;
