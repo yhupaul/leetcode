c57af8ff58b112f29d96040fcb28b0cc457b70a2
yhupaul <yhupaul@hotmail.com>
2022-09-17 20:59:00 -0700

    practice for presentation

--- twoSum.js
+++ twoSum.js
@@ -1,14 +1,34 @@
-const twoSum = function(nums, target) {
-  let result = [];
-  for (let i=0; i<nums.length; i++) {
-      for (let j= i+1; j<nums.length; j++){
-          if (nums[i] + nums[j] ===  target){
-              result.push(i);
-              result.push(j);
-          }
-      }
-      }
-  return result;
-  }
+// const twoSum = function(nums, target) {
+//   let result = [];
+//   for (let i=0; i<nums.length; i++) {
+//       for (let j= i+1; j<nums.length; j++){
+//           if (nums[i] + nums[j] ===  target){
+//               result.push(i);
+//               result.push(j);
+//           }
+//       }
+//       }
+//   return result;
+//   }
 
-console.log(twoSum([2,7,11,15], 9));
+// console.log(twoSum([2,7,11,15], 9));
+
+/*
+time complexity
+space complexity
+
+HashMap
+
+nums = [2, 7, 11, 15]
+target = 9
+
+map = {}
+i = 0
+value = 1
+*/
+
+
+ const twoSum = function(nums, target) {
+    let hashMap = {};
+
+ }
--- validParentheses.js
+++ validParentheses.js
@@ -1,39 +1,71 @@
-// isValid = s => {
-//   // Create a stack
-//   let stack = [];
-//   // Loop through each element in the string
-//   for (i=0; i<s.length; i++){
-//     let char = stack[stack.length -1]
-//     // If you encounter a starting bracket, push it onto the stack
-//     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
-//       stack.push(s[i]);
-//       // Pop the opening bracket off the stack,
-//       // if there is a corresponding closing bracket in the string
-//     } else if (
-//       (char === "(" && s[i] === ")") || 
-//       (char === "{" && s[i] === "}") ||
-//       (char === "[" && s[i] === "]") 
-//     ) {
-//       stack.pop();
-//     } else return false;
-//   }
-//   // Check empty stack
-//   return stack.length ? false : true;
+
+/* 
+example:
+(), {}, [] -> valid
+{()}, [{}], {[()]}, ({})[] -> valid
+[, (, { -> invalid
+{(}, {]} -> invalid
+
+     i    
+({})[]
+
+stack [ ]
+
+time complexity = o(n)
+space complexity = o(n)
+*/
+
+
+
+// const isValid = function(s) {
+//     const hashMap = { "(": ")", "{": "}", "[": "]" }
+//     let stack = []
+
+//     for(let ele of s) {
+//         if(hashMap[ele]) {
+//             stack.push(hashMap[ele])
+//         } else if(stack.length > 0 && stack[stack.length - 1] === ele) {
+//             stack.pop()
+//         } else {
+//             return false
+//         }
+//     }
+//     return !(stack.length)
 // }
 
-// // finally I am able to understand it
+/*
+ examples
+    (), {}, [] -> valid
+    ([]), {[()]}, {[]}() -> valid
 
-var isValid = function(s) {
-  const hashMap = { "(":")", "{":"}", "[":"]" };
-  const stack = [];
-  for(let ch of s) {
-      if(hashMap[ch]) {
-          stack.push(hashMap[ch]);
-      } else if (stack.length > 0 && stack[stack.length - 1] === ch) {
-          stack.pop();
-      } else {
-          return false;
-      }
-  }
-  return !stack.length;
-};
+    (, ), {[}], ()) -> invalid 
+
+        
+    {[]}()
+
+    stack = []
+
+    time complexity = 0(N)
+    space complexity = 0(N)
+*/
+
+const isValid = function(str) {
+    const hashMap={"(": ")", "[": "]", "{": "}"}
+    let stack = []
+
+    for(let ch of s) {
+        //ch is an opening bracket
+        if(hashMap[ch]) {
+            stack.push(hashMap[ch])
+        }
+        // ch is an closing bracket and it matches with the last ele of stack arr
+        else if(stack.length>0 && stack[stack.length -1] === ch) {
+            stack.pop()
+        }
+        // ch is an closing bracket and it does not match with the last ele of stack arr
+        else {
+            return false
+        }
+    }
+    return stack.length === 0
+}
